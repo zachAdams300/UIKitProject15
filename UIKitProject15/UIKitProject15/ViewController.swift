69291e72b71b4b44c259df9ab7935af0f05998f4
zachAdams300 <ztaboy2008@hotmail.com>
2024-01-13 17:47:51 -0500

    UIKitProject15

---
+++ UIKitProject15/UIKitProject15/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  UIKitProject15
+//
+//  Created by Zachary Adams on 1/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var tapButton: UIButton!
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = view.center
+        
+        view.addSubview(imageView)
+    }
+
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIButton.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+}
+
